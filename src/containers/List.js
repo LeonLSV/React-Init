14a768234184fe39fcb51bf191b25206c7e47ce5
Ramiro Contreras León <racramiro@gmail.com>
2020-10-08 14:10:33 -0500

    Proyecto funcionando con Bootswatch y Css

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,6 +1,25 @@
 import React from "react";
+// import { render } from "react-dom";
 import Card from "../components/card/Card";
 
-export const List = () => <Card />;
+class List extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch("../../assets/data.json");
+    const resJSON = await res.json();
+    this.setState({ data: resJSON });
+  }
+  render() {
+    return this.state.data.map((movie) => {
+      return <Card movie={movie} />;
+    });
+  }
+}
 
 export default List;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDom from "react-dom";
 
 import List from "./containers/List";
 
+import "bootswatch/dist/lux/bootstrap.min.css";
+
 ReactDom.render(<List />, document.getElementById("root"));
